75655878b8a0851e0e77c6bb274c9d522d3e306c
integral_digi <ojaduagreat@gmail.com>
2024-03-27 00:07:25 +0100

    NEW entry for portfolio

--- app/components/PortfolioData.ts
+++ app/components/PortfolioData.ts
@@ -30,7 +30,7 @@ export const projectData: ProjectDataProps[] = [
         year: "2023",
         link: "https://middletrust-v2.vercel.app",
         description: "Middletrust is a cutting-edge platform designed to revolutionize online transactions and financial management. With its user-friendly interface and robust security features, Middletrust ensures seamless and secure financial interactions for users worldwide.",
-        github: "https://github.com/integral-digi/middletrust"
+        github: "https://bit.ly/43wKTDH"
     },
     {
         id: 1,
@@ -46,7 +46,7 @@ export const projectData: ProjectDataProps[] = [
         year: "2021",
         link: "https://travvelbaby-tau.vercel.app",
         description: "Travvelbaby is your ultimate travel companion, offering comprehensive travel planning tools and resources. From itinerary customization to real-time updates, Travvelbaby simplifies the travel experience, making every journey memorable and hassle-free.",
-        github: "https://github.com/integral-digi/travvelbaby"
+        github: "https://bit.ly/43Cr51u"
     },
     {
         id: 2,
@@ -62,7 +62,7 @@ export const projectData: ProjectDataProps[] = [
         year: "2023",
         link: "https://test-ace.com",
         description: "Test-Ace is a sophisticated online testing platform designed to streamline assessment processes. With its advanced analytics and customizable test creation tools, Test-Ace empowers educators and organizations to conduct assessments with ease and accuracy.",
-        github: "https://github.com/integral-digi/testace"
+        github: "https://bit.ly/3ISnNhe"
     },
     {
         id: 3,
@@ -76,7 +76,7 @@ export const projectData: ProjectDataProps[] = [
         photoTwo: "",
         photoThree: "",
         year: "2023",
-        link: "https://toodle.vercel.app",
+        link: "https://toodleapp.vercel.app",
         description: "Toodle is a productivity powerhouse, offering intuitive task management and collaboration features. Whether you're managing personal projects or coordinating with a team, Toodle helps you stay organized and efficient, every step of the way.",
         github: "https://github.com/integral-digi/toodle"
     },
@@ -92,9 +92,9 @@ export const projectData: ProjectDataProps[] = [
         photoTwo: "",
         photoThree: "",
         year: "2023",
-        link: "https://daintree.vercel.app",
-        description: "Daintree is a visually stunning portfolio platform crafted for creatives and professionals. With its customizable templates and seamless integration, Daintree showcases your work in the best light possible, helping you stand out in a competitive landscape.",
-        github: "https://github.com/integral-digi/daintree"
+        link: "https://daintree-shop.vercel.app",
+        description: "Daintree is a visually stunning online store crafted for creatives and techies. Shop the latest gadgets, and equipments from a brand that seeks to serve users where brands like Amazon falls short",
+        github: "https://bit.ly/3xd2d4w"
     },
     {
         id: 5,
@@ -124,8 +124,24 @@ export const projectData: ProjectDataProps[] = [
         photoTwo: "",
         photoThree: "",
         year: "2023",
-        link: "https://oracel.vercel.app",
+        link: "https://oracel-omor.vercel.app",
         description: "Oracle is a comprehensive data analytics platform designed to unlock insights and drive informed decision-making. With its powerful analytics tools and intuitive dashboards, Oracle empowers businesses to harness the full potential of their data assets.",
+        github: ""
+    },
+    {
+        id: 7,
+        name: "MasteryEcom",
+        tags: ["design", "frontend"],
+        photo: "/assets/mecom.png",
+        logo: "/assets/mecom.svg",
+        intro: "",
+        midsection: "",
+        outro: "",
+        photoTwo: "",
+        photoThree: "",
+        year: "2023",
+        link: "https://masteryecom.com/#metric",
+        description: "MasteryEcom is a feature-rich e-commerce solution tailored for modern businesses. From seamless checkout experiences to robust inventory management, MasteryEcom offers everything you need to build and scale your online store with confidence.",
         github: ""
     },
 ];
